b09b27ab701d25538084546f62d7a5217c0ba6a2
Jim Jeffers <jim@sumocreations.com>
2009-06-01 17:43:21 -0700

    Added role_requirement

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,5 @@ class Article < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   
-  validates_presence_of :author_id, :on => :create, :message => "must be specified"
+  validates_presence_of :user_id, :on => :create, :message => "must be specified"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090601205921) do
+ActiveRecord::Schema.define(:version => 20090602004244) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
@@ -88,6 +88,18 @@ ActiveRecord::Schema.define(:version => 20090601205921) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "roles", :force => true do |t|
+    t.string "name"
+  end
+
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
+  add_index "roles_users", ["role_id"], :name => "index_roles_users_on_role_id"
+  add_index "roles_users", ["user_id"], :name => "index_roles_users_on_user_id"
 
   create_table "sports", :force => true do |t|
     t.string   "name"
