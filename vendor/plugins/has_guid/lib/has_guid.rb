848bd8fbb49656e215d65e852c577c591ccba53e
Jim Jeffers <jim@sumocreations.com>
2009-06-06 15:08:01 -0700

    Added unique guid support.

--- vendor/plugins/has_guid/lib/has_guid.rb
+++ vendor/plugins/has_guid/lib/has_guid.rb
@@ -11,6 +11,7 @@ module ActiveRecord
           unless included_modules.include? InstanceMethods
             include ActiveRecord::Has::Guid::InstanceMethods
             class_inheritable_accessor :has_guid_field
+            validates_uniqueness_of :guid
           end
           self.has_guid_field = field.to_sym
           define_method("#{has_guid_field.to_s}=".to_sym) do |method_name, *args|
@@ -30,7 +31,15 @@ module ActiveRecord
         # Creates a guid based off an attrbute
         def make_guid(attribute)
           unless (field = read_attribute(attribute.to_sym)).nil?
-            field.split(" ").each{|i| i.downcase!; i.gsub!(/[^a-zA-Z0-9]/,'') }.join("-")
+            field = field.split(" ").each{|i| i.downcase!; i.gsub!(/[^a-zA-Z0-9]/,'') }.join("-")
+            if self.class.find_by_guid(field)
+              tmp = "#{field}-#{i=1}"
+              while !self.class.find_by_guid(tmp).nil?
+                tmp = "#{field}-#{i+=1}"
+              end
+              field = tmp
+            end
+            field
           else
             field = nil
           end
