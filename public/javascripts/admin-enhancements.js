47b20ad1b17355fd585c7a87b8a4a234586894fc
Jim Jeffers <jim@sumocreations.com>
2009-06-16 15:34:47 -0700

    Resolved date selector issues.

--- public/javascripts/admin-enhancements.js
+++ public/javascripts/admin-enhancements.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-   
    $('#article_published_at').hide();
    $('#published_at_option').show();
    if($('#article_published_at').val() == "") {
@@ -17,7 +16,17 @@ $(document).ready(function() {
       $('#edit_published_at').parent().hide();
       $('#edit_published_at').parent().next().show();
       $('#published_at_date').datepicker();
+      
+      $('#published_at_date, #published_at_time').change(function() {
+         var formattedDate = $('#published_at_date').val();
+         $('#article_published_at').val(formattedDate.substr(6,4)+'-'+formattedDate.substr(0,2)+'-'+formattedDate.substr(3,2)+' '+$('#published_at_time').val());
+         alert($('#article_published_at').val());
+      });
    }
+   $('#published_at_date, #published_at_time').change(function() {
+      var formattedDate = $('#published_at_date').val();
+      $('#article_published_at').val(formattedDate.substr(6,4)+'-'+formattedDate.substr(0,2)+'-'+formattedDate.substr(3,2)+' '+$('#published_at_time').val());
+   });
    $('.accordian').accordion();
    $('.datepicker').datepicker();
 });
